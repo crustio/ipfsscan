071f92a08a11700422f5deebbc75ea790015c0e8
Yashiro <1161569428@qq.com>
2021-12-02 11:17:05 +0800

    Remove unused config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   trailingSlash: true,
-  // webpack: (config, { isServer }) => {
-  //   if (!isServer) {
-  //     config.resolve.fallback.tls = false;
-  //     config.resolve.fallback.fs = false;
-  //   }
-
-  //   return config
-  // },
 }
